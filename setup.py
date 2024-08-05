c96ddd62b4ca67e07fb060f50af53c58f5331171
Lukas Garbas <lukasgarba@gmail.com>
2024-08-06 01:11:05 +0200

    Setup and licence

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+def read_requirements():
+    with open('requirements.txt') as req:
+        return req.read().strip().split('\n')
+
+
+setup(
+    name='transformer-ranker',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Rank transformer models for NLP tasks using transferability measures',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author='Lukas Garbas',
+    author_email='lukasgarba@gmail.com',
+    install_requires=read_requirements(),
+)
