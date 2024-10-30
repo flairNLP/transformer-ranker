d06ce1e17fd9117e754ae256c276ab66ec4eacd2
Max Ploner <plonerma@maxploner.de>
2024-10-30 15:03:07 +0100

    Simplified dataset preprocessing test case

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,47 +1,37 @@
-from typing import List, Tuple, Type, Union
-
 import pytest
 import torch
 from datasets import Dataset
 from transformer_ranker.datacleaner import DatasetCleaner
 
 
-def load_datasets(dataset_type: str, num_datasets: Union[str, int] = "all") -> Tuple[List[str], Type, Type]:
-    """Try loading and preparing different datasets"""
-    dataset_map = {
-        'token': (
-            ["conll2003", "wnut_17"],
-            "token classification", list
-        ),
-        'text': (
-            ["trec", "stanfordnlp/sst2", "hate_speech18"],
-            "text classification", str
-        ),
-        'text_pair': (
-            ["yangwang825/sick", "SetFit/rte"],
-            "text classification", str
-        )
-    }
+test_datasets = [
+    # Token datasets
+    ("token classification", "conll2003"),
+    ("token classification", "wnut_17"),
 
-    datasets, task_type, sentence_type = dataset_map[dataset_type]
-    if isinstance(num_datasets, int):
-        datasets = datasets[:num_datasets]
+    # Text classification
+    ("text classification", "trec"),
+    ("text classification", "stanfordnlp/sst2"),
+    ("text classification", "hate_speech18"),
 
-    return datasets, task_type, sentence_type
+    # Text-pair classification
+    ("text classification", "yangwang825/sick"),
+    ("text classification", "SetFit/rte"),
+]
 
+@pytest.mark.parametrize("task_type,dataset_name", test_datasets)
+def test_datacleaner(task_type, dataset_name):
 
-def validate_dataset(
-        preprocessor,
-        dataset_name: str,
-        dataset: Dataset,
-        expected_task_type: Type,
-        sentence_type: Type
-):
+    preprocessor = DatasetCleaner(dataset_downsample=0.2)
+    dataset = preprocessor.prepare_dataset(dataset_name)
+
+    # Test dataset preprocessing
     assert isinstance(dataset, Dataset), f"Dataset '{dataset_name}' is not a valid Dataset object"
 
-    assert preprocessor.task_type == expected_task_type, \
-        (f"Task type mismatch: expected '{expected_task_type}', got '{preprocessor.task_type}'"
-         f"in dataset '{dataset_name}'")
+    assert preprocessor.task_type == task_type, (
+        f"Task type mismatch: expected '{task_type}', got '{preprocessor.task_type}'"
+        f"in dataset '{dataset_name}'"
+    )
 
     # Make sure text and label columns were found
     assert preprocessor.text_column is not None, f"Text column not found in dataset {dataset_name}"
@@ -52,35 +42,33 @@ def validate_dataset(
     assert isinstance(sentences, list) and len(sentences) > 0, (
         "Sentences/tokens list is empty in dataset %s", dataset_name
     )
-    assert all(isinstance(sentence, sentence_type) for sentence in sentences), \
-        (f"Incorrect sentence/token type in dataset '{dataset_name}', all expected to be '{sentence_type}' "
-         f"but some sentences have different type")
 
-    if sentence_type == str:
-        # For text and text pair classification, make sure there's no empty strings
-        assert all(sentence != "" for sentence in sentences), f"Empty sentence found in dataset {dataset_name}"
+    # Ensure the sentences are in the correct format (str for text-classification, List[str] for token-level)
+    if task_type == "text classification":
+        for sentence in sentences:
+            assert isinstance(sentence, str), (
+                f"Incorrect sentence type in dataset '{dataset_name}', all expected to be str "
+                f"but some sentences have different type ({type(sentence)})."
+            )
 
-    if sentence_type == list:
-        # For token classification, make sure there is no empty tokens
-        assert all(sentence != [] for sentence in sentences), f"Empty token list found in dataset {dataset_name}"
-        # Check that no empty strings exist within the token lists
-        assert all(all(token != "" for token in sentence) for sentence in sentences), \
-            f"Empty token found within a sentence in dataset {dataset_name}"
+            # For text and text pair classification, make sure there's no empty strings
+            assert sentence != "", f"Empty sentence found in dataset {dataset_name}"
+
+    elif task_type == "token classification":
+        for sentence in sentences:
+            # For token classification, make sure there is no empty lists of tokens
+            assert len(sentence) >= 0,  f"Empty token list found in dataset {dataset_name}"
+
+            # Check that no empty strings exist within the token lists
+            for token in sentence:
+                assert isinstance(token, str), f"Non-str token ({type(token)}) found within a sentence in dataset {dataset_name}."
+                assert len(token) >= 0, f"Empty token found within a sentence in dataset {dataset_name}."
+
+    else:
+        msg = f"Uncrecognized task type '{task_type}'."
+        raise KeyError(msg)
 
     # Test the label column in each dataset
     labels = preprocessor.prepare_labels(dataset)
     assert isinstance(labels, torch.Tensor) and labels.size(0) > 0, "Labels tensor is empty"
     assert (labels >= 0).all(), f"Negative label found in dataset {dataset_name}"
-
-
-@pytest.mark.parametrize("dataset_type", ["text", "token", "text_pair"])
-def test_datacleaner(dataset_type):
-    datasets, task_type, sentence_type = load_datasets(dataset_type, "all")
-
-    # Loop through all test datasets, down sample them to 0.2
-    for dataset_name in datasets:
-        preprocessor = DatasetCleaner(dataset_downsample=0.2)
-        dataset = preprocessor.prepare_dataset(dataset_name)
-
-        # Test dataset preprocessing
-        validate_dataset(preprocessor, dataset_name, dataset, task_type, sentence_type)
