be4db4d08d5966f0836b62dd4d29416053a4e3f6
Lukas Garbas <lukasgarba@gmail.com>
2025-02-21 20:52:16 +0100

    Add more test datasets

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,60 @@
 import pytest
 import torch
-from datasets import load_dataset
+from datasets import Dataset, load_dataset
 from sklearn import datasets
 from transformers import AutoModel
 
 
 @pytest.fixture(scope="session")
 def small_language_models():
-    """Use two tiny models for testing"""
+    """Use two small models for fast testing."""
     return (
         AutoModel.from_pretrained("prajjwal1/bert-tiny"),
-        AutoModel.from_pretrained("google/electra-small-discriminator")
+        AutoModel.from_pretrained("google/electra-small-discriminator"),
+    )
+
+
+@pytest.fixture(scope="session")
+def sample_dataset():
+    """One dummy custom dataset."""
+    return Dataset.from_dict(
+        {
+            "text": ["whatsup", "quick", "", "datasets", "test"],
+            "text_pair": ["Python programming", "loading", "student", "humboldt", "focus"],
+            "label": [1, 0, 2, 1, 2],
+            "extra": [100, 200, 300, 400, 500],
+        }
     )
 
 
 @pytest.fixture(scope="session")
 def conll():
+    """Ner dataset, load once and keep."""
     return load_dataset("conll2003")
 
 
 @pytest.fixture(scope="session")
+def wnut():
+    """One more ner dataset"""
+    return load_dataset("wnut_17")
+
+
+@pytest.fixture(scope="session")
 def trec():
+    """One text classification"""
     return load_dataset("trec")
+
+
+@pytest.fixture(scope="session")
+def sick():
+    """Sick has text pairs"""
+    return load_dataset("yangwang825/sick")
+
+
+@pytest.fixture(scope="session")
+def stsb():
+    """Sts has floats as labels (regression)"""
+    return load_dataset("glue", "stsb")
 
 
 @pytest.fixture(scope="session")
@@ -29,7 +62,4 @@ def iris_dataset():
     iris = datasets.load_iris()
     data = torch.tensor(iris["data"], dtype=torch.float32)
     data[142] += torch.tensor([0, 0, 0, 0.01])  # Ensure no exact duplicates
-    return {
-        "data": data,
-        "target": torch.tensor(iris["target"], dtype=torch.float32)
-    }
+    return {"data": data, "target": torch.tensor(iris["target"], dtype=torch.float32)}
