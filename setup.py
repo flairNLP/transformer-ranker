e3da5d80926763841c1f9c12e2bedc0d456e2fbe
Lukas Garbas <lukasgarba@gmail.com>
2025-03-16 21:05:12 +0100

    Update pyproject.toml

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
-from setuptools import setup, find_packages
+import setuptools
 
-
-def read_requirements():
-    with open('requirements.txt') as req:
-        return req.read().strip().split('\n')
-
-
-setup(
-    name='transformer-ranker',
-    version='0.1.2',
-    packages=find_packages(),
-    description='Efficiently find the best-suited language model (LM) for your NLP task',
-    long_description=open('README.md').read(),
-    long_description_content_type="text/markdown",
-    author='Lukas Garbas',
-    author_email='lukasgarba@gmail.com',
-    url="https://github.com/flairNLP/transformer-ranker",
-    install_requires=read_requirements(),
-    license='MIT',
-    python_requires=">=3.9",
-)
+setuptools.setup()
